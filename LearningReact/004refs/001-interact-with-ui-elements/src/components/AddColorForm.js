2770affd6a77761fc961935d3e655009ee6a6237
Amandeep Chawla <adchawla@gmail.com>
2017-06-16 10:04:43 +0530

    implementing Inverse Data Flow

--- LearningReact/004refs/001-interact-with-ui-elements/src/components/AddColorForm.js
+++ LearningReact/004refs/001-interact-with-ui-elements/src/components/AddColorForm.js
@@ -3,11 +3,28 @@
 import React, {Component} from 'react';
 
 export default class AddColorForm extends Component {
+    constructor() {
+        super();
+        // We needed to add a constructor to this ES6 component class because we moved submit to its own function.
+        // With ES6 component classes, we must bind the scope of the component to any methods that need to access
+        // that scope with this.
+        this.submit = this.submit.bind(this);
+    }
+
+    submit(e) {
+        const {_title, _color} = this.refs;
+        e.preventDefault();
+        this.props.onNewColor(_title.value, _color.value);
+        _title.value = '';
+        _color.value = '#000000';
+        _title.focus();
+    }
+
     render() {
         return (
-            <form onSubmit={e=>e.preventDefault()}>
-                <input type='text' placeholder='color title...' required/>
-                <input type='color' required/>
+            <form onSubmit={this.submit}>
+                <input ref='_title' type='text' placeholder='color title...' required/>
+                <input ref='_color' type='color' required/>
                 <button>ADD</button>
             </form>
         );
--- LearningReact/004refs/001-interact-with-ui-elements/src/index.js
+++ LearningReact/004refs/001-interact-with-ui-elements/src/index.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import AddColorForm from './components/AddColorForm';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
