8715eb77071093a6c0cc00b7285d3097a23e9f14
Amandeep Chawla <adchawla@gmail.com>
2017-06-16 07:10:55 +0530

    Adding an AddColorForm Component

--- LearningReact/004refs/001-interact-with-ui-elements/src/index.js
+++ LearningReact/004refs/001-interact-with-ui-elements/src/index.js
@@ -1,8 +1,13 @@
+/*jshint esversion: 6 */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AddColorForm from './components/AddColorForm';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.React = React;
+
+ReactDOM.render(<AddColorForm />, document.getElementById('root'));
 registerServiceWorker();
