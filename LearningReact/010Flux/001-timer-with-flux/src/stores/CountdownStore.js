30d86849a5e548d9e9a3485913b66ce553ce3311
Amandeep Chawla <adchawla@gmail.com>
2017-06-20 20:52:36 +0530

    Correcting the problem with count

--- LearningReact/010Flux/001-timer-with-flux/src/stores/CountdownStore.js
+++ LearningReact/010Flux/001-timer-with-flux/src/stores/CountdownStore.js
@@ -3,25 +3,30 @@ import { EventEmitter } from 'events';
 export default class CountdownStore extends EventEmitter {
     constructor( count = 5, dispatcher ) {
         super();
-        this.count = count;
+        // eslint-disable-next-line no-underscore-dangle
+        this._count = count;
         this.dispatcherIndex = dispatcher.register( this.dispatch.bind( this ) );
     }
 
     get count() {
-        return this.count;
+        // eslint-disable-next-line no-underscore-dangle
+        return this._count;
     }
 
     dispatch( payload ) {
         const { type, count } = payload.action;
         switch ( type ) {
         case 'TICK':
-            this.count = this.count - 1;
-            this.emit = ( 'TICK', this.count );
+        // eslint-disable-next-line no-underscore-dangle
+            this._count = this._count - 1;
+            this.emit( 'TICK', this.count );
             return true;
 
         case 'RESET':
-            this.count = count;
-            this.emit( 'RESET', this.count );
+            // eslint-disable-next-line no-underscore-dangle
+            this._count = count;
+            // eslint-disable-next-line no-underscore-dangle
+            this.emit( 'RESET', this._count );
             return true;
 
         default:
