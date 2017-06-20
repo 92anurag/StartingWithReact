962af61cb5c7b2ee771a0e7cece237a4c15d30fa
Amandeep Chawla <adchawla@gmail.com>
2017-06-20 20:50:41 +0530

    Correcting the problem. in place of registering a function to be called on onClick event, actually reset(10) was getting called.

--- LearningReact/010Flux/001-timer-with-flux/src/components/Countdown.js
+++ LearningReact/010Flux/001-timer-with-flux/src/components/Countdown.js
@@ -3,13 +3,13 @@ import React from 'react';
 // eslint-disable-next-line react/prop-types
 const Countdown = ( { count, tick, reset } ) => {
     if ( count ) {
-        setTimeout( () => tick(), 1000 );
+        setTimeout( () => tick( count ), 1000 );
     }
 
     return ( count ) ?
         <h1>{ count }</h1> :
         // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-        <div onClick={ reset( 10 ) }>
+        <div onClick={ () => reset( 10 ) }>
             <span>CELEBRATE!!!!</span>
             <span>(click to start over)</span>
         </div>;
