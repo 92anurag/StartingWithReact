84d43a29665af4921f020e0f06fa8dc4150a5661
Amandeep Chawla <adchawla@gmail.com>
2017-06-20 19:48:44 +0530

    There is only ever one dispatcher, and it represents the air traffic control part of this design pattern. The dispatcher takes the action, packages it with some information about where the action was generated, and sends it on to the appropriate store or stores that will handle the action. When handleAction is invoked with an action, it is dispatched along with some data about where the action originated. When a store is created, it is registered with the dispatcher and starts listening for actions. When an action is dispatched it is handled in the order that it was received and sent to the appropriate stores.

---
+++ LearningReact/010Flux/001-timer-with-flux/src/dispatchers/CountdownDispatcher.js
@@ -0,0 +1,12 @@
+import Dispatcher from 'flux';
+
+export default class CountdownDispatcher extends Dispatcher {
+    handleAction( action ) {
+        // eslint-disable-next-line no-console
+        console.log( 'dispatching action:', action );
+        this.dispatch( {
+            source: 'VIEW_ACTION',
+            action,
+        } );
+    }
+}
