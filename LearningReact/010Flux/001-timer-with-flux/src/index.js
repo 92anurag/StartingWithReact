eb13f9511e0816e3c546ed4ec1627e192212345c
Amandeep Chawla <adchawla@gmail.com>
2017-06-20 20:54:06 +0530

    Putting everything together.

--- LearningReact/010Flux/001-timer-with-flux/src/index.js
+++ LearningReact/010Flux/001-timer-with-flux/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import CountdownDispatcher from './dispatchers/CountdownDispatcher';
+import CountdownActions from './actions/CountdownActions';
+import Countdown from './components/Countdown';
+import CountdownStore from './stores/CountdownStore';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appDispatcher = new CountdownDispatcher();
+const actions = CountdownActions( appDispatcher );
+const store = new CountdownStore( 10, appDispatcher );
+
+const render = ( count ) => {
+    ReactDOM.render(
+        <Countdown count={ count } { ...actions } />,
+        document.getElementById( 'root' ),
+    );
+};
+
+const rerender = () => render( store.count );
+
+store.on( 'TICK', rerender );
+store.on( 'RESET', rerender );
+rerender();
+
 registerServiceWorker();
