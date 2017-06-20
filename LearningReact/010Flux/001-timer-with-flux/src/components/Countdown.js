2843810f0d9eab0e713c9bde2ef95778b12af23c
Amandeep Chawla <adchawla@gmail.com>
2017-06-20 19:36:54 +0530

    The countdown view takes in the count to display as a property. It also receives a couple of functions, tick and reset. When this view renders it will display the count, unless the count is 0, in which case it will display a message instructing the user to “CELEBRATE!!!” If the count is not 0, then the view sets a timeout, waits for a second, and invokes a TICK. When the count is 0, this view will not invoke any other action creators until a user clicks the main div and triggers a reset. This resets the count to 10 and starts the whole countdown process over again.

---
+++ LearningReact/010Flux/001-timer-with-flux/src/components/Countdown.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const Countdown = ( { count, tick, reset } ) => {
+    if ( count ) {
+        setTimeout( () => tick(), 1000 );
+    }
+
+    return ( count ) ?
+        <h1>{ count }</h1> :
+        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+        <div onClick={ reset( 10 ) }>
+            <span>CELEBRATE!!!!</span>
+            <span>(click to start over)</span>
+        </div>;
+};
+
+export default Countdown;
