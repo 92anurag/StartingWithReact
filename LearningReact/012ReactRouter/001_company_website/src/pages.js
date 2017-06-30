fdb43a90e9de6f98866bb1118389c5c9225f1d52
Amandeep Chawla <adchawla@gmail.com>
2017-06-30 10:52:21 +0530

    We do not expect our users to navigate the website by typing routes into the location bar. The react-router-dom provides a Link component that we can use to create browser links.
    Let’s modify the home page to contain a navigation menu with a link for each route. Now users can access every internal page from the home page by clicking on a link. The browser’s back button will take them back to the home page.

--- LearningReact/012ReactRouter/001_company_website/src/pages.js
+++ LearningReact/012ReactRouter/001_company_website/src/pages.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home = () => (
     <section className="home">
         <h1>[Home Page]</h1>
+        <nav>
+            <Link to="about">[About]</Link>
+            <Link to="events">[Events]</Link>
+            <Link to="products">[Products]</Link>
+            <Link to="contact">[Contact Us]</Link>
+        </nav>
     </section>
 );
 
