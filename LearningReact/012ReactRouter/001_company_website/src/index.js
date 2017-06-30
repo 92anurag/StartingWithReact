fbf81a4564e587b61736ee27c607bc86de4089ce
Amandeep Chawla <adchawla@gmail.com>
2017-06-30 10:59:27 +0530

    Trying to Add 404 page for resources not present

--- LearningReact/012ReactRouter/001_company_website/src/index.js
+++ LearningReact/012ReactRouter/001_company_website/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 
-import { Home, About, Events, Products, Contact } from './pages';
+import { Home, About, Events, Products, Contact, Whoops404 } from './pages';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -14,6 +14,7 @@ ReactDOM.render(
             <Route path="/events" component={ Events } />
             <Route path="/products" component={ Products } />
             <Route path="/contact" component={ Contact } />
+            <Route component={ Whoops404 } />
         </div>
     </HashRouter>
     , document.getElementById( 'root' ) );
--- LearningReact/012ReactRouter/001_company_website/src/pages.js
+++ LearningReact/012ReactRouter/001_company_website/src/pages.js
@@ -36,3 +36,9 @@ export const Contact = () => (
         <h1>[Contact Us]</h1>
     </section>
 );
+
+export const Whoops404 = () => (
+    <div className="whoops-404">
+        <h1>Resource not found</h1>
+    </div>
+);
