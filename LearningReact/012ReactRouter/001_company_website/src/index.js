e933ddb2fee6b3b5f7c5cf98ada494395dbbd719
Amandeep Chawla <adchawla@gmail.com>
2017-06-30 11:01:40 +0530

    Since we only want to display the Whoops404 component when no other Route matches, we need to use the Switch component. The Switch component only displays the first route that matches. This assures that only one of these routes will be rendered. If none of the locations match a Route, the last route—the one that does not contain a path property—will be displayed.

--- LearningReact/012ReactRouter/001_company_website/src/index.js
+++ LearningReact/012ReactRouter/001_company_website/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import { Home, About, Events, Products, Contact, Whoops404 } from './pages';
 import './index.css';
@@ -9,12 +9,14 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
     <HashRouter>
         <div className="main" >
-            <Route exact path="/" component={ Home } />
-            <Route path="/about" component={ About } />
-            <Route path="/events" component={ Events } />
-            <Route path="/products" component={ Products } />
-            <Route path="/contact" component={ Contact } />
-            <Route component={ Whoops404 } />
+            <Switch>
+                <Route exact path="/" component={ Home } />
+                <Route path="/about" component={ About } />
+                <Route path="/events" component={ Events } />
+                <Route path="/products" component={ Products } />
+                <Route path="/contact" component={ Contact } />
+                <Route component={ Whoops404 } />
+            </Switch>
         </div>
     </HashRouter>
     , document.getElementById( 'root' ) );
